60f2ba3e726c25b5434ae83df2f62c296247b7c1
NandoSchlemper <bkschlemper@gmail.com>
2025-03-13 12:50:31 -0300

    [Feature]: Lidando com o server igual gente

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { env } from '@/config/env.ts'
 import { app } from '@/server/index.ts'
-import { serve } from '@hono/node-server'
 
-//console.log(env.HONO_PORT)
-
-//export default {
-//  fetch: app.fetch,
-//  port: 3030,
-//}
-
-serve({
+export default {
   fetch: app.fetch,
   port: env.HONO_PORT,
-})
+}
+
 console.log(`Server running in port ${env.HONO_PORT}`)
